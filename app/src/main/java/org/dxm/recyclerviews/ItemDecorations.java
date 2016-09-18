b25e6536e59b68cf465ff190dcd55da6cdd146ef
jiajing <jia.jing@xiaoyi.com>
2016-09-18 16:28:18 +0800

    feat(ItemDecorations): add ItemDecoration builder

---
+++ app/src/main/java/org/dxm/recyclerviews/ItemDecorations.java
@@ -0,0 +1,58 @@
+package org.dxm.recyclerviews;
+
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ants on 9/18/16.
+ */
+
+public class ItemDecorations {
+    private static class MarginDecoration extends RecyclerView.ItemDecoration {
+        @NonNull final Rect rect;
+
+        private MarginDecoration(@NonNull Rect rect) {
+            this.rect = rect;
+        }
+
+        @Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            outRect.set(rect);
+        }
+    }
+
+    public MarginBuilder marginBuilder() {
+        return new MarginBuilder();
+    }
+
+    public RecyclerView.ItemDecoration margin(int margin) {
+        return marginBuilder().left(margin).top(margin).right(margin).bottom(margin).build();
+    }
+
+    public static class MarginBuilder {
+        private final Rect rect = new Rect();
+        public MarginBuilder left(int left) {
+            rect.left = left;
+            return this;
+        }
+        public MarginBuilder top(int top) {
+            rect.top = top;
+            return this;
+        }
+
+        public MarginBuilder right(int right) {
+            rect.right = right;
+            return this;
+        }
+
+        public MarginBuilder bottom(int bottom) {
+            rect.bottom = bottom;
+            return this;
+        }
+
+        public RecyclerView.ItemDecoration build() {
+            return new MarginDecoration(rect);
+        }
+    }
+}
